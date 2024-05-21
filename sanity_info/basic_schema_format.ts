b17e188f0d2abac911c067013d3f8e5ab43911b3
mansi104-ai <mkb.kalra@gmail.com>
2024-05-21 12:09:43 +0530

    basic sanity files for config and schema added

---
+++ sanity_info/basic_schema_format.ts
@@ -0,0 +1,57 @@
+import { Rule } from "postcss";
+import { title } from "process";
+import { validation } from "sanity";
+import { defineType, defineArrayMember, defineField } from "sanity";
+export default {
+  name: "post",
+  type: "document",
+  title: "Post",
+  fields: [
+    defineField({
+      name: "title",
+      title: "Title",
+      type: "string",
+      validation: (Rule) =>
+        Rule.required().max(50).error("This field is required"),
+    }),
+
+    //slug field
+
+    defineField({
+      name: "slug",
+      type: "slug",
+      title: "Slug",
+      options: {
+        source: "title",
+        maxLength: 50,
+      },
+    }),
+
+    defineField({
+      name: "summary",
+      type: "text",
+      title: "summary",
+      validation: (Rule) => Rule.required(),
+    }),
+
+    defineField({
+      name: "image",
+      type: "image",
+      title: "Image",
+    }),
+    // {
+    //     name :'gender',
+    //     type : 'string',
+    //     title : 'Gender',
+    //     options :{
+    //         list : [
+    //             {title : 'Male', value:'male'},
+    //             {title : 'Female', value : 'female'},
+    //         ],
+    //         layout : 'radio',
+    //         direction : 'horizontal',
+    //     }
+
+    // },
+  ],
+};
---
+++ sanity_info/next_config_file.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+};
+
+// module.exports = {
+//     logging: {
+//       fetches: {
+//         fullUrl: true,
+//       },
+//     },
+//   }
+
+export default nextConfig;
